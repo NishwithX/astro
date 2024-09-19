d63dafdacb0c9a121fe5747cda57873f59102ccd
Nishwith <nihalnishwith987@gmail.com>
2024-09-20 00:22:16 +0530

    first dd

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://nishwithx.github.io/',
-  base: 'astro',
-  experimental: {
-    contentLayer: true,
-  },
-});
